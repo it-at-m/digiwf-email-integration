f9986af4a0f1a03847bd5be7b4ba6729c4126e8c
axd4 <101194240+axd4@users.noreply.github.com>
2022-05-11 15:27:24 +0200

    Worked in PR comments
    * added unit test for validation method

--- digiwf-email-integration/src/test/java/io/muenchendigital/digiwf/email/integration/domain/service/MailingServiceTest.java
+++ digiwf-email-integration/src/test/java/io/muenchendigital/digiwf/email/integration/domain/service/MailingServiceTest.java
@@ -1,18 +1,49 @@
-//package io.muenchendigital.digiwf.email.integration.domain.service;
-//
-//import io.muenchendigital.digiwf.email.integration.domain.model.Attachment;
-//import org.junit.jupiter.api.Test;
-//
-//import static org.hamcrest.MatcherAssert.assertThat;
-//import static org.hamcrest.Matchers.is;
-//
-//class MailingServiceTest {
-//
-//
-//    @Test
-//    void isAttachmentPathAndDocumentStorageNotBlank() {
-//        final Attachment attachment = new Attachment();
-//
-//        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(false));
-//    }
-//}
+package io.muenchendigital.digiwf.email.integration.domain.service;
+
+import io.muenchendigital.digiwf.email.integration.domain.model.Attachment;
+import io.muenchendigital.digiwf.s3.integration.client.repository.DocumentStorageFileRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@ExtendWith(MockitoExtension.class)
+public class MailingServiceTest {
+
+    @Mock
+    private DocumentStorageFileRepository documentStorageFileRepository;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    private MailingService mailingService;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.mailingService = new MailingService(this.javaMailSender, "from@example.com", this.documentStorageFileRepository);
+
+    }
+
+    @Test
+    void isAttachmentPathAndDocumentStorageNotBlank() {
+        final Attachment attachment = new Attachment();
+
+        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(false));
+        attachment.setAttachmentPath("");
+        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(false));
+        attachment.setDocumentStorageUrl("");
+        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(false));
+        attachment.setAttachmentPath("Path");
+        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(false));
+        attachment.setAttachmentPath("");
+        attachment.setDocumentStorageUrl("Url");
+        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(false));
+        attachment.setAttachmentPath("Path");
+        assertThat(mailingService.isAttachmentPathAndDocumentStorageNotBlank(attachment), is(true));
+    }
+}
