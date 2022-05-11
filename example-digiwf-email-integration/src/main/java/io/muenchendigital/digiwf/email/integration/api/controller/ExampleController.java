e47dd7dc07dbf888e2b7a66d977e006cb4c05aa2
axd4 <101194240+axd4@users.noreply.github.com>
2022-05-11 10:10:29 +0200

    Change example attachments

--- example-digiwf-email-integration/src/main/java/io/muenchendigital/digiwf/email/integration/api/controller/ExampleController.java
+++ example-digiwf-email-integration/src/main/java/io/muenchendigital/digiwf/email/integration/api/controller/ExampleController.java
@@ -1,6 +1,7 @@
 package io.muenchendigital.digiwf.email.integration.api.controller;
 
 import io.muenchendigital.digiwf.email.integration.domain.exception.MissingInformationMailException;
+import io.muenchendigital.digiwf.email.integration.domain.model.Attachment;
 import io.muenchendigital.digiwf.email.integration.domain.model.Mail;
 import io.muenchendigital.digiwf.email.integration.domain.service.MailingService;
 import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.service.PayloadSenderService;
@@ -9,6 +10,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -47,9 +51,11 @@ public class ExampleController {
         mail.setSubject("Test1234");
         mail.setBody("Hallo test123");
         mail.setReplyTo("");
-//        final List<String> attachmentPaths = new ArrayList<>();
-//        attachmentPaths.add("test/picture.jpg");
-//        mail.setAttachmentPaths(attachmentPaths);
+        final Attachment attachment = new Attachment();
+        attachment.setDocumentStorageUrl("http://localhost:8086");
+        attachment.setAttachmentPath("test/picture.jpg");
+        final List<Attachment> attachments = new ArrayList<>();
+        mail.setAttachments(attachments);
         return mail;
     }
 
