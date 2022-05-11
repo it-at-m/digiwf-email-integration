4ffa0681daf944926e9936b30b43057143662344
axd4 <101194240+axd4@users.noreply.github.com>
2022-05-11 10:15:09 +0200

    Small fixes (#5)
    * Added docs folder and some files in it
    
    * Added documentation.md and used absolute paths
    
    * Added architecture section in readme
    
    * Fixed architecture.png and added element-template
    
    * Small fixes
    
    * Fixed prefix of autoconfig properties
    * Moved mailRouter to the starter
    
    * Translated ElementTemplate to english

--- digiwf-email-integration/src/main/java/io/muenchendigital/digiwf/email/integration/configuration/MailConfiguration.java
+++ digiwf-email-integration/src/main/java/io/muenchendigital/digiwf/email/integration/configuration/MailConfiguration.java
@@ -1,20 +1,15 @@
 package io.muenchendigital.digiwf.email.integration.configuration;
 
-import io.muenchendigital.digiwf.spring.cloudstream.utils.api.streaming.infrastructure.RoutingCallback;
-import org.springframework.cloud.function.context.MessageRoutingCallback;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import javax.mail.MessagingException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
 @Configuration
 public class MailConfiguration {
-
-    public static final String TYPE_HEADER_SEND_MAIL_FROM_EVENT_BUS = "sendMailFromEventBus";
 
     public JavaMailSender getJavaMailSender(
             final String host,
@@ -36,17 +31,6 @@ public class MailConfiguration {
         mailSender.setJavaMailProperties(props);
         mailSender.testConnection();
         return mailSender;
-    }
-
-    /**
-     * Override the custom router of the digiwf-spring-cloudstream-utils. We only have one type we need to map.
-     *
-     * @return the custom router
-     */
-    public MessageRoutingCallback mailRouter() {
-        final Map<String, String> typeMappings = new HashMap<>();
-        typeMappings.put(TYPE_HEADER_SEND_MAIL_FROM_EVENT_BUS, TYPE_HEADER_SEND_MAIL_FROM_EVENT_BUS);
-        return new RoutingCallback(typeMappings);
     }
 
 }
